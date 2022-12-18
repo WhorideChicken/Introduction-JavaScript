5569f39f4a21403f800872b0345b51c728ea5373
Jin <97941811+WhorideChicken@users.noreply.github.com>
2022-12-18 16:33:11 +0900

    first Reat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+
+//컴포넌트는 일정의 UI 조각입니다. 쉽게 재사용도 가능합니다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+//리액트 컴포넌트를 만들 때 아래와 같이 import해야합니다.
+import React from "react";
+
+function Hello(){
+    return <div>안녕하세요</div>
+}
+
+export default Hello;
+
